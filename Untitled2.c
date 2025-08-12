387f34cc67bfb78f992b6423663df8f4849dfb6e
Evabatada2512 <evabatada7@gmail.com>
2025-08-12 17:32:28 +0530

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+printf("Enter two numbers\n");
+int a,b;
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+printf("a is greater number");
+}
+else
+{
+printf("b is greater number");
+}
+return 0;
+}
