c47f204d66d4116e55ddbb8b62a9774e531edfc0
Evabatada2512 <evabatada7@gmail.com>
2025-08-12 17:46:27 +0530

    Add files via upload

--- Untitled2.c
+++ Untitled2.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 int main()
 {
-printf("Enter two numbers\n");
-int a,b;
-scanf("%d%d",&a,&b);
-if(a>b)
+printf("Enter three numbers\n");
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
 {
 printf("a is greater number");
 }
-else
+if(b>c)
 {
 printf("b is greater number");
+}
+else
+{
+printf("c is greater number");
 }
 return 0;
 }
